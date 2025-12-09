6e9732da6ca5f2c6b3cf96c531d78146e929aff2
Surface Laptop 2 <mentors@team1058.com>
2025-12-09 18:22:17 -0500

    first commit of the day

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -492,7 +492,7 @@ public class Drivetrain extends SwerveDrivetrain<TalonFX, TalonFX, CANcoder> imp
         || (Math.abs(getRotation3d().getMeasureY().baseUnitMagnitude()) > 0.15)));
   }
   public boolean getIsDrivetrainMoving(){
-    return (Math.abs(this.getCurrentSpeeds().vxMetersPerSecond) + Math.abs(this.getCurrentSpeeds().vyMetersPerSecond)> 0.1);
+    return (Math.abs(this.getCurrentSpeeds().vxMetersPerSecond) + Math.abs(this.getCurrentSpeeds().vyMetersPerSecond)) > 0.1;
   }
 
   public Trigger getAlignedTrigger(
