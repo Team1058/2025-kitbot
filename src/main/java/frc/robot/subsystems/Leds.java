cbeea7ed45d436fdc28e7359565fb581482e1d57
Surface Laptop 2 <mentors@team1058.com>
2025-12-16 20:50:35 -0500

    add labling of Sha (unfinished)

--- src/main/java/frc/robot/subsystems/Leds.java
+++ src/main/java/frc/robot/subsystems/Leds.java
@@ -4,7 +4,9 @@ import static edu.wpi.first.units.Units.Meters;
 import static edu.wpi.first.units.Units.MetersPerSecond;
 import static edu.wpi.first.units.Units.Seconds;
 
-import edu.wpi.first.units.measure.Distance;
+import java.util.HashMap;
+import java.util.Map;
+
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.AddressableLEDBufferView;
@@ -12,14 +14,13 @@ import edu.wpi.first.wpilibj.LEDPattern;
 import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.RobotContainer;
-import frc.robot.subsystems.*;
 
 public class Leds extends SubsystemBase{
     private static final int kPort = 0;
     private static final int kLength = 64;
     private final AddressableLED ledStrip;
     private final AddressableLEDBuffer ledBuffer;
+    private final AddressableLEDBufferView ShaBuffer;
     private final AddressableLEDBufferView topBuffer;
     private final AddressableLEDBufferView bottomBuffer;
 
@@ -38,7 +39,8 @@ public class Leds extends SubsystemBase{
     public Leds(){
         ledBuffer = new AddressableLEDBuffer(kLength);
         ledStrip = new AddressableLED(kPort);
-        topBuffer = ledBuffer.createView(32, 63);
+        ShaBuffer = ledBuffer.createView(52, 63);
+        topBuffer = ledBuffer.createView(32, 51);
         bottomBuffer = ledBuffer.createView(0, 31).reversed();
         ledStrip.setLength(kLength);
         ledStrip.start();
@@ -53,6 +55,42 @@ public class Leds extends SubsystemBase{
         rainbowBarf = rainbowBase.scrollAtAbsoluteSpeed(MetersPerSecond.of(0.1), Meters.of(1.0/120));
         topStripPattern = redBreathe;
         bottomStripPattern = redBreathe;
+
+    }
+
+    public LEDPattern colorSegmentFromCharacter(String last4OfSha){
+
+        Color[] char1Colors = getColorsFromCharacter(last4OfSha.charAt(0));
+        Color[] char2Colors = getColorsFromCharacter(last4OfSha.charAt(1));
+        Color[] char3Colors = getColorsFromCharacter(last4OfSha.charAt(2));
+        Color[] char4Colors = getColorsFromCharacter(last4OfSha.charAt(3));
+        Map<Double, Color> colorsMap = new HashMap<Double, Color>();
+        colorsMap.put(0.0, char1Colors[0]);
+        colorsMap.put(0.083, char1Colors[1]);
+        colorsMap.put(0.166, Color.kBlack);
+        colorsMap.put(.25, char2Colors[0]);
+        colorsMap.put(0.333, char2Colors[1]);
+        colorsMap.put(0.416, Color.kBlack);
+        colorsMap.put(.5,char3Colors[0]);
+        colorsMap.put(0.583, char3Colors[1]);
+        colorsMap.put(0.666, Color.kBlack);
+        colorsMap.put(0.75,char4Colors[0]);
+        colorsMap.put(0.833, char4Colors[1]);
+        colorsMap.put(0.916, Color.kBlack);
+
+
+
+        // return LEDPattern.steps(Map.of(0, char1Colors[0], 0.083, char1Colors[1], 0.166, Color.kBlack,
+        // .25, char2Colors[0], 0.333, char2Colors[1], 0.416, Color.kBlack,
+        // .5,char3Colors[0], 0.583, char3Colors[1], 0.666, Color.kBlack,
+        // 0.75,char4Colors[0], 0.833, char4Colors[1], 0.916, Color.kBlack));
+
+        return LEDPattern.steps(colorsMap);
+    }
+
+    public Color[] getColorsFromCharacter(char character){
+        Color[] colorsForCharacter = {, Color.kBlue};
+        return colorsForCharacter;
     }
 
     @Override
@@ -69,7 +107,27 @@ public class Leds extends SubsystemBase{
 LED plan:   individual strip length: 32 / 19 inches
             4 strips, 2 each side
             on the outside of wood part of intake
-            mirrored LEDs
-            top LED: is moving? status
-            bottom LED: is intaking? status
+W = White
+R = Red
+G = Green
+Bl = Blue
+Br = Brown
+P = Pink
+
+0: W W
+1: W R
+2: W G
+3: W Bl
+4: W Br
+5: W P
+6: R R
+7: R G
+8: R Bl
+9: R Br
+A: R P
+B: G G
+C: G Bl
+D: G Br
+E: G P
+F: Bl Bl
 */
